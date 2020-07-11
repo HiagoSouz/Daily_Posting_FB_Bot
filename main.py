f1a844f276d4c8a328e1fcaab05147a6d06f2bbb
Rodalife <hiago.rafael.souza@hotmail.com>
2020-07-11 08:25:03 -0300

    Final Version

---
+++ main.py
@@ -0,0 +1,27 @@
+import random
+import facebook
+import time
+import schedule
+
+def geraFrase():
+    frases = ["Mas é óbvio que sim", "Com certeza", "O ritual foi cumprido hoje", "Si mi amigo", "Sì mio amico",
+                  "Ja mein Freund", "да мой друг", "Claro que sim né", "Mandou com um duplo carpado", "Hoje ele mandou 2 em sequência",
+                  "~Não", "Opa","Sim, meu patrão", "Mandou assim que acordou", "Mandou por video-chamada"]
+    numera = random.randint(1, 15)
+    return frases[numera]
+
+def postToFacebook(token,message="Hello"):
+    graph = facebook.GraphAPI(token)
+    post_id = graph.put_photo(image = open('400.jpg', 'rb'), message= message)["post_id"]
+    print(f"Postou com sucesso n° {post_id}")
+
+def job():
+    token = "" ##Insira seu Token de acesso aqui
+    message = geraFrase()
+    postToFacebook(token,message)
+
+if __name__  == '__main__':
+    schedule.every(20).hours.do(job).run()
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
